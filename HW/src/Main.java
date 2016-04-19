e0d0a3ab0a173b34eff082e6bd74255a56aa78e0
natan00013 <so_os@ro.ru>
2016-04-19 12:10:49 +0300

    added test ArithmeticException

--- HW/src/Main.java
+++ HW/src/Main.java
@@ -1,11 +1,17 @@
 
 public class Main {
 
- 
+	static int i;
+	
 	public static void main(String[] args) {
 		 
 		System.out.print("Current time is");
 		System.out.println(ShowTime.show());
+		
+		 try { i = 5/0; }
+	        catch(Exception e) {
+	             if (e instanceof ArithmeticException) {  System.out.println(e); }
+	     }
 		
 	}
 
