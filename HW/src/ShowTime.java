dcaeb46474ca7345ccffdfc569f222bb00d4df03
natan00013 <so_os@ro.ru>
2016-04-19 11:39:22 +0300

    added ShowTime

---
+++ HW/src/ShowTime.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class ShowTime {
+ 
+	public Date show(){
+		
+		return GregorianCalendar.getInstance().getTime();
+		
+	}
+
+}
